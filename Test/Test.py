561830a3463a1704080a11336962c07a715ef455
Thiripura29 <Thiripura29>
2024-08-16 09:54:48 +0000

    code updated

--- Test/Test.py
+++ Test/Test.py
@@ -37,4 +37,40 @@ We can also use display() to display  the data in tabular format
 
 # COMMAND ----------
 
-show.(df)
+
+
+# COMMAND ----------
+
+df.show(20,truncate=False,vertical=False)
+
+"""
+     The show() function in Spark DataFrame is used to display the contents of the DataFrame in a tabular format.
+     Spark will display a certain number of rows from the DataFrame, usually the first 20 rows by default
+     Here are some common arguments you can pass 
+     numRows: Specifies the number of rows to display. By default, it displays the first 20 rows.
+     truncate: Specifies whether to truncate the displayed data if it's too wide. Truncation means cutting off some characters to fit the data within the display width. By default, it's set to True
+     vertical: Specifies whether to display the output in a vertical format. By default, it's set to False, meaning the data is displayed horizontally.
+     Usage:
+     n: An alias for numRows.
+     df.show(n=100, truncate=False, vertical=True)  # Display without truncating and in vertical format
+
+
+"""
+
+# COMMAND ----------
+
+Options  = {
+    "header": "true","inferSchema": "true"
+}
+
+df = spark \
+    .read \
+    .format('csv') \
+    .options(**Options) \
+    .load('/Volumes/databricks_catalog/s3_test/healthcare_1/csv files/csv/2024_05_08T04_08_53Z/allergies.csv') \
+     
+"""
+    create dictionary for all options to pass to the options function
+"""
+
+display(df)
